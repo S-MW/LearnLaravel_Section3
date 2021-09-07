ed8855b4d6431e67c763637388ac8fa0c6d6adf5
S-MW <72529306+S-MW@users.noreply.github.com>
2021-09-07 14:59:54 +0300

    add controller & modle with migration

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +21,8 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+
+Route::resource('Listings', Controllers\ListingsController::class);
 
 require __DIR__.'/auth.php';
